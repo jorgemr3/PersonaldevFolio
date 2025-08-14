628f4cdc99473c4d9e30a5b34432e5d5061d2970
jorgemr3 <jorgemr103@gmail.com>
2025-08-14 15:09:08 -0600

    Add Firebase configuration and custom 404 error page
    - Created .firebaserc to set the default Firebase project.
    - Added firebase.json for hosting configuration, specifying the public directory and files to ignore.
    - Implemented a custom 404.html page for handling not found errors with a user-friendly message and styling.

--- src/model/Proyecto.js
+++
@@ -1,9 +0,0 @@
-export class Proyecto {
-    constructor(id, titulo, descripcion, link, photo) {
-      this.id = id;
-      this.titulo = titulo;
-      this.descripcion = descripcion;
-      this.link = link;
-      this.photo = photo
-    }
-  }
